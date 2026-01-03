a4552c7399c9fcf10386f3b4a36c4487f85ccb81
yurii.lychak <yurii.lychak@gmail.com>
2026-01-03 10:10:35 +0200

    Update path

--- packages/web-interface/src/i18n.ts
+++ packages/web-interface/src/i18n.ts
@@ -4,7 +4,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 export default async function fetchLocales() {
     return new Promise<void>(async resolve => {
-        const localesRaw = await fetch(`${window.location.origin}/assets/en-us.json`);
+        const localesRaw = await fetch('dist/en-us.json');
         const translation = await localesRaw.json();
         const defaultLocale = 'en-US';
 
